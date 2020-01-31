820018234a9679f29f8bd53393cf9355070457dd
clarachene <c_chene@digital-campus.net>
2020-01-31 10:44:50 +0000

    création de fichier dans views/ + modification du fichier server.js

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//OS est un utilitaire node qui va nous servir à afficher le nom de notre raspberry
+const os = require("os");
+//MustacheExpress est notre moteur de template
+const mustacheExpress = require('mustache-express');
+
+//Configuration du moteur de template
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+//Ici on dit au serveur de servir les fichiers statiques depuis le dossier /public
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
-  response.send('Requête reçue...')
+  //Ici on indique que nous voulons transformer notre fichier index.mustache en HTML
+  response.render('index');
 })
 
 app.get('/hello/:name', (request, response) => {
-  response.send('Hello, '+request.params.name+'!');
+  //De la même manière nous transformons notre fichier hello.mustache en HTML en passant des paramètres.
+  response.render('hello', {name: request.params.name});
 })
 
 app.listen(port, (err) => {
@@ -15,5 +30,6 @@ app.listen(port, (err) => {
     return console.log('Erreur du serveur : ', err)
   }
 
-  console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://localhost:'+port);
+  //On utilise l'utilitaire OS pour récupérer le nom de notre raspberry.
+  console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);
 })
